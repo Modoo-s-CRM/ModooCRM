c97341c441c4c106d9881ae705dc3e468cb8cd35
thethe133 <thethe133@naver.com>
2022-11-01 15:51:01 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/modoocrm/modoocrm/ModooCrmApplication.java
@@ -0,0 +1,13 @@
+package com.modoocrm.modoocrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModooCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModooCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modoocrm/modoocrm/ModooCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modoocrm.modoocrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModooCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
